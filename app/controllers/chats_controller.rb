8b2fcc17586f9071f3b4dad805e400755d28f6da
komi53 <kxs53kuro@outlook.jp>
2020-09-07 04:29:15 +0000

    indent fix

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,21 +1,21 @@
 class ChatsController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
   def show
-    @user = User.find(params[:id])
-    rooms = current_user.user_rooms.pluck(:room_id)
-    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+  @user = User.find(params[:id])
+  rooms = current_user.user_rooms.pluck(:room_id)
+  user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
 
-    unless user_rooms.nil?
-      @room = user_rooms.room
-    else
-      @room = Room.new
-      @room.save
-      UserRoom.create(user_id: current_user.id, room_id: @room.id)
-      UserRoom.create(user_id: @user.id, room_id: @room.id)
-    end
-    @chats = @room.chats.page(params[:page]).without_count.per(8).order(created_at: "DESC")
-    @chats_name = @room.user_rooms.where.not(user_id: current_user)
-    @chat = Chat.new(room_id: @room.id)
+  unless user_rooms.nil?
+    @room = user_rooms.room
+  else
+    @room = Room.new
+    @room.save
+    UserRoom.create(user_id: current_user.id, room_id: @room.id)
+    UserRoom.create(user_id: @user.id, room_id: @room.id)
+  end
+  @chats = @room.chats.page(params[:page]).without_count.per(8).order(created_at: "DESC")
+  @chats_name = @room.user_rooms.where.not(user_id: current_user)
+  @chat = Chat.new(room_id: @room.id)
   end
   def create
     @chat = current_user.chats.new(chat_params)
